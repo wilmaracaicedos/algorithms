8e7e723e9e0c7a780a6f7a7898bd9b477a1e0218
Wilmar Caicedo <wilmar.caicedo@farmappweb.com>
2022-12-05 21:14:43 -0500

    chunk exercise

--- arrayChunck/index.test.ts
+++ arrayChunck/index.test.ts
@@ -11,7 +11,7 @@
 import "jest";
 import { chunk } from ".";
 
-describe("Array Chunk", () => {
+describe.only("Array Chunk", () => {
   it("Should exist chunk function", () => {
     expect(typeof chunk).toBe("function");
   });
@@ -21,8 +21,16 @@ describe("Array Chunk", () => {
       [3, 4],
     ]);
   });
-  it.only("chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]", () => {
-    console.log(chunk([1, 2, 3, 4, 5], 2));
-    expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+  it("chunk([1, 2, 3], 2) --> [[ 1, 2, 3], [3]]", () => {
+    expect(chunk([1, 2, 3], 2)).toEqual([[1, 2], [3]]);
+  });
+  it("chunk([1, 2, 3, 4, 5], 3) --> [[ 1, 2, 3], [4, 5]]", () => {
+    expect(chunk([1, 2, 3, 4, 5], 3)).toEqual([
+      [1, 2, 3],
+      [4, 5],
+    ]);
+  });
+  it("chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]", () => {
+    expect(chunk([1, 2, 3, 4, 5], 10)).toEqual([[1, 2, 3, 4, 5]]);
   });
 });
--- arrayChunck/index.ts
+++ arrayChunck/index.ts
@@ -1,32 +1,32 @@
-export function chunk2(array: Array<number>, size: number) {
+export function chunk(array: Array<number>, size: number) {
   const response = [];
-  let count = 0;
+  let count = 1;
   let subArray = [];
   let index = 0;
   while (index < array.length) {
-    // 1 --- 2
-    const element = array[index]; // 2 --- 3
-    if (count < size) {
-      // 2 < 2
-      subArray.push(element); // 1, 2
-      count++; // 2
-      index++; // 2
-    } else {
-      console.log(...subArray);
-      response.push([...subArray]); // [1, 2]
-      count = 0;
-      subArray.length = 0;
-      // console.log(index);
+    const element = array[index];
+    if (count <= size) {
+      subArray.push(element);
+      count++;
+      index++;
+      if (size == subArray.length) {
+        response.push([...subArray]);
+        count = 1;
+        subArray.length = 0;
+      }
+      if (index == array.length && subArray.length > 0) {
+        response.push([...subArray]);
+      }
     }
   }
   return response;
 }
 
-export function chunk(array: Array<number>, size: number) {
-  const response = [];
-  while (array.length) {
-    const element = array.splice(0, size);
-    response.push(element);
-  }
-  return response;
-}
+// export function chunk(array: Array<number>, size: number) {
+//   const response = [];
+//   while (array.length) {
+//     const element = array.splice(0, size);
+//     response.push(element);
+//   }
+//   return response;
+// }
